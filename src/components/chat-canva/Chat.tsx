47d05d3cd37fc500b80dc04295c6c770530442c0
CarlosMartel7 <fernandinhoaira@gmail.com>
2024-04-25 23:14:45 -0300

    fix: minor changes

--- src/components/chat-canva/Chat.tsx
+++ src/components/chat-canva/Chat.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Message from "../constructors/Message";
-import { deleteMessageFunction, editChangesFunction, message, sendMessageFunction} from "../constructors/message/Message.types";
+import { deleteMessageFunction, editChangesFunction, message, sendMessageFunction } from "../constructors/message/Message.types";
 import ChatInput from "../constructors/chat-input/ChatInput";
 import ChatHeader from "../constructors/chat-header/ChatHeader";
 import '../../index.css'
@@ -31,15 +31,15 @@ function Chat({ devMode, chat, user, date, friend, deletedLabel, blockInput, edi
   const [messages, setMessages] = useState<message[]>(chat);
   const screenRef = useRef<HTMLDivElement | null>(null)
 
-  const saveChangesFunc = (format: string, content: message, index: number, type: 'deleted' | 'edit'): void=> {
+  const saveChangesFunc = (format: string, content: message, index: number, type: 'deleted' | 'edit'): void => {
     setMessages((msg: message[]) => {
       let returnedMessages = [...msg]
-      if(type === 'deleted'){
+      if (type === 'deleted') {
         returnedMessages[index] = content
         return returnedMessages
       }
 
-      if (type === 'edit'){
+      if (type === 'edit') {
         returnedMessages[index] = content
         return returnedMessages
       }
@@ -47,38 +47,38 @@ function Chat({ devMode, chat, user, date, friend, deletedLabel, blockInput, edi
       return returnedMessages
     });
 
-    if(editMessageFunction && type === 'edit'){
+    if (editMessageFunction && type === 'edit') {
       editMessageFunction(format, content, index);
     }
 
-    if(deleteMessageFunction && type === 'deleted'){
+    if (deleteMessageFunction && type === 'deleted') {
       deleteMessageFunction(index, content)
     }
-    
+
   };
 
   useEffect(() => {
-    if(messages.length != chat.length){
+    if (messages.length != chat.length) {
       setMessages(chat)
     }
   }, [chat])
 
   useEffect(() => {
-    if(screenRef.current){
+    if (screenRef.current) {
       screenRef.current.scrollTop = screenRef.current.scrollHeight
     }
   }, [messages])
 
   return (
     <section className="w-full h-full relative overflow-hidden bg-chat-bg">
-        <ChatHeader friend={friend} handleGoBackButton={handleGoBackButton}/>
-        <div className="w-full h-[80vh] message-area-h h-80vh overflow-y-auto pb-6" id="main-chat" ref={screenRef}>
-          <span className="fixed bg-stone-300 left-1/2 transform -translate-x-1/2 p-2 rounded-lg mt-1">{date}</span>
+      <ChatHeader friend={friend} handleGoBackButton={handleGoBackButton} />
+      <div className="w-full h-[80vh] message-area-h h-80vh overflow-y-auto pb-6" style={{ height: blockInput ? '100%' : '80vh' }} id="main-chat" ref={screenRef}>
+        <span className="fixed bg-stone-300 left-1/2 transform -translate-x-1/2 p-2 rounded-lg mt-1">{date}</span>
         {messages.map((thisMessage: message, index: number) => (
-          <Message message={thisMessage} index={index} deletedLabel={deletedLabel} saveChangesFunc={saveChangesFunc} key={thisMessage.order} hasDelete={hasDelete != undefined ? hasDelete : true}  hasEdit={hasEdit != undefined ? hasEdit : true} />
+          <Message message={thisMessage} index={index} deletedLabel={deletedLabel} saveChangesFunc={saveChangesFunc} key={thisMessage.order} hasDelete={hasDelete != undefined ? hasDelete : true} hasEdit={hasEdit != undefined ? hasEdit : true} />
         ))}
-        </div>
-        <ChatInput setMessages={setMessages} messages={messages} blockInput={blockInput} initialSend={initialSend} hasAttached={hasAttach != undefined ? hasAttach : true} sendMessageFunction={sendMessageFunction}/>
+      </div>
+      <ChatInput setMessages={setMessages} messages={messages} blockInput={blockInput} initialSend={initialSend} hasAttached={hasAttach != undefined ? hasAttach : true} sendMessageFunction={sendMessageFunction} />
     </section>
   );
 }
